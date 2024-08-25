92807cfa0ddb1f227521a49e11bf5da0f53d28d6
apple <apple@apples-MacBook-Pro.local>
2024-08-25 17:00:01 +0530

    First

--- src/App.js
+++ src/App.js
@@ -1,25 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
+// Filename - App.js
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import axios from "axios";
+
+import React, { Component } from "react";
+
+class App extends Component {
+    state = {
+
+        selectedFile: null,
+    };
+
+
+    onFileChange = (event) => {
+
+        this.setState({
+            selectedFile: event.target.files[0],
+        });
+    };
+
+
+    onFileUpload = () => {
+
+        const formData = new FormData();
+
+
+        formData.append(
+            "myFile",
+            this.state.selectedFile,
+            this.state.selectedFile.name
+        );
+
+
+        console.log(this.state.selectedFile);
+
+        axios.post("/", formData);//API URL pettu ikkada
+    };
+
+    fileData = () => {
+        if (this.state.selectedFile) {
+            return (
+                <div>
+                    <h2>File Details:</h2>
+                    <p>
+                        File Name:{" "}
+                        {this.state.selectedFile.name}
+                    </p>
+
+                    <p>
+                        File Type:{" "}
+                        {this.state.selectedFile.type}
+                    </p>
+
+                    <p>
+                        Last Modified:{" "}
+                        {this.state.selectedFile.lastModifiedDate.toDateString()}
+                    </p>
+                </div>
+            );
+        } else {
+            return (
+                <div>
+                    <p>upload the file to extract the info</p>
+                </div>
+            );
+        }
+    };
+
+    render() {
+        return (
+            <div style={{marginLeft:"30%"}}>
+                <h1>the NIRF info extractor</h1>
+                <div >
+                    <input
+                        type="file"
+                        onChange={this.onFileChange}
+                    />
+                    <button onClick={this.onFileUpload}>
+                        Upload pdf
+                    </button>
+                </div>
+                {this.fileData()}
+            </div>
+        );
+    }
 }
-
+// 
 export default App;
